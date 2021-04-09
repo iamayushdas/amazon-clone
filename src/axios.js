74e8a925a73a3a0591b6b99d995d1e9b227b6e8b
iamayushdas <ayush24das@gmail.com>
2021-04-09 15:02:20 +0530

    complete amazon-clone

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-81eee/us-central1/api'
+    baseURL: 'https://us-central1-clone-81eee.cloudfunctions.net/api'
 });
 
 export default instance;
+
+// http://localhost:5001/clone-81eee/us-central1/api
